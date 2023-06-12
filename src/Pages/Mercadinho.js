8b91428f07bf40274f15289f2577830f4875f123
Anacxx <anacarolina.clcx@gmail.com>
2023-06-12 11:53:46 -0300

    sdf

--- src/Pages/Mercadinho.js
+++ src/Pages/Mercadinho.js
@@ -1,39 +1,47 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { handleCart} from "../Router/cordinator";
+import { handleCart } from "../Router/cordinator";
 import CardFrutas from "../components/CardFrutas";
 import frutaria from "../frutaria.json";
 import styled from "styled-components";
 
-export default function Mercadinho(props) {
+export default function Mercadinho({carrinho, setCarrinho}) {
     const [frutas, setFrutas] = useState(frutaria.frutaria);
     const navigate = useNavigate();
-
+    
     function comprar(id) {
-        // const i = carrinho.findIndex((item) => item.id === id);
-        // console.log(i);
-        // if (i !== -1) {
-        //   carrinho[i] = { ...carrinho[i], amount: carrinho[i].amount + 1 };
-        // } else {
-        //   const frutaEncontrada = frutas.find((fruta) => fruta.id === id);
-        //   const novaFruta = { ...frutaEncontrada, amount: 1 };
-        //   const novaLista = [...carrinho, (carrinho[1] = novaFruta)];
+        const i = carrinho.findIndex((item) => item.id === id);
+        if (i !== -1) {
+            carrinho[i] = { ...carrinho[i], amount: carrinho[i].amount + 1 };
+        } else {
+            const frutaEncontrada = frutas.find((fruta) => fruta.id === id);
+            const novaFruta = { ...frutaEncontrada, amount: 1 };
+            const novaLista = [...carrinho, (carrinho[1] = novaFruta)];
 
-        //   setCarrinho(novaLista);
+            setCarrinho(novaLista);
+        }
     }
 
 
 
-return (
-    <MercadinhoContainer>
-        <h1>Mercadinho</h1>
-        <button onClick={() => handleCart(navigate)}>Vá para Carrinho </button>
-        <button>Cadastro de Frutas </button>
-        <FrutasContainer>
-            <CardFrutas />
-        </FrutasContainer>
-    </MercadinhoContainer>
-);
+    return (
+        <MercadinhoContainer>
+            <h1>Mercadinho</h1>
+            <button onClick={() => handleCart(navigate)}>Vá para Carrinho </button>
+            <button>Cadastro de Frutas </button>
+            <FrutasContainer>
+                {frutas.map((fruta) => {
+                    return <CardFrutas
+                        id={fruta.id}
+                        name={fruta.name}
+                        image={fruta.url}
+                        price={fruta.price}
+                        comprar={comprar}
+                    />
+                })}
+            </FrutasContainer>
+        </MercadinhoContainer>
+    );
 }
 const FrutasContainer = styled.section`
   display: flex;
